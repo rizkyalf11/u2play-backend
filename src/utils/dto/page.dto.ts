0b8dd9167e1c5db1afd88345bd58560eb34222df
ehancup <mondokskuy19@gmail.com>
2025-08-12 10:24:08 +0700

    fix: dto pagination

--- src/utils/dto/page.dto.ts
+++ src/utils/dto/page.dto.ts
@@ -1,15 +1,19 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsInt, IsOptional } from 'class-validator';
 
 export class PageRequestDto {
+  @ApiPropertyOptional({ example: 1, description: 'Nomor halaman' })
   @IsInt()
   @Type(() => Number)
   page = 1;
 
+  @ApiPropertyOptional({ example: 20, description: 'Jumlah data per halaman' })
   @IsInt()
   @Type(() => Number)
   pageSize = 20;
 
+  
   @IsInt()
   @IsOptional()
   limit;
