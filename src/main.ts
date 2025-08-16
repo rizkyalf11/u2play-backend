e1a8fd9e6bf676245b2fa9234624bb97d801d4e1
rizky a <107133530+rizkyalf11@users.noreply.github.com>
2025-08-16 16:14:39 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,24 @@ import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
 
-  const config = new DocumentBuilder()
-    .setTitle('U2PLAY API')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'apiKey',
-        in: 'header',
-        name: 'Authorization',
-      },
-      'token',
-    )
-    .addTag('gaming')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  // const config = new DocumentBuilder()
+  //   .setTitle('U2PLAY API')
+  //   .setDescription('The cats API description')
+  //   .setVersion('1.0')
+  //   .addBearerAuth(
+  //     {
+  //       type: 'apiKey',
+  //       in: 'header',
+  //       name: 'Authorization',
+  //     },
+  //     'token',
+  //   )
+  //   .addTag('gaming')
+  //   .build();
+  // const documentFactory = () => SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, documentFactory);
   app.set('trust proxy', true);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -35,8 +36,8 @@ async function bootstrap() {
   app.enableCors();
   await app.listen(process.env.APP_PORT!);
   console.log('run at', process.env.APP_PORT!);
-  console.log(
-    'documentation at -> http://localhost:' + process.env.APP_PORT! + '/api',
-  );
+  // console.log(
+  //   'documentation at -> http://localhost:' + process.env.APP_PORT! + '/api',
+  // );
 }
 bootstrap();
