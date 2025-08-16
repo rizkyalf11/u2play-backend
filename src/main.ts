5d23d1582abcab6bcf4d9b2ef1841c2b25018522
rizky a <107133530+rizkyalf11@users.noreply.github.com>
2025-08-16 14:12:30 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('U2PLAY API')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'apiKey',
-        in: 'header',
-        name: 'Authorization',
-      },
-      'token',
-    )
-    .addTag('gaming')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-  app.set('trust proxy', true);
+  // const config = new DocumentBuilder()
+  //   .setTitle('U2PLAY API')
+  //   .setDescription('The cats API description')
+  //   .setVersion('1.0')
+  //   .addBearerAuth(
+  //     {
+  //       type: 'apiKey',
+  //       in: 'header',
+  //       name: 'Authorization',
+  //     },
+  //     'token',
+  //   )
+  //   .addTag('gaming')
+  //   .build();
+  // const documentFactory = () => SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, documentFactory);
+  // app.set('trust proxy', true);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -32,11 +31,14 @@ async function bootstrap() {
     }),
   );
   app.useStaticAssets(path.join(__dirname, '..', 'public'));
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  });
   await app.listen(process.env.APP_PORT!);
-  console.log('run at', process.env.APP_PORT!);
-  console.log(
-    'documentation at -> http://localhost:' + process.env.APP_PORT! + '/api',
-  );
+  // console.log('run at', process.env.APP_PORT!);
+  // console.log(
+  //   'documentation at -> http://localhost:' + process.env.APP_PORT! + '/api',
+  // );
 }
 bootstrap();
